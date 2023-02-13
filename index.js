7dabc379320999f7d234a89f9f986e95634cd13b
Lordsoro <98828393+Lordsoro@users.noreply.github.com>
2023-02-13 09:51:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const port = 5000
+const port = 5000;
 
-app.get('/', (req, res) => {
-    console.log('funciona')
-})
+app.get("/", (req, res) => {
+  console.log("funciona");
+});
 
 app.listen(() => {
-    console.log(`Server running on port: ${port}`)
-})
+  console.log(`Server running on port: ${port}`);
+});
